c152bd5c124cba2a4531d1bde257ef2a1abe1391
matekuna92 <kunamate66@gmail.com>
2022-10-27 21:41:29 +0200

    home, movieId, notfound Route

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 import { GlobalStyle } from './GlobalStyle';
 import Header from './components/Header';
 import Home from './components/Home';
+import Movie from './components/Movie';
+import NotFound from './components/NotFound';
 
-function App() {
-  return (
-    <div className="App">
+const App = () => (
+  //  <div className="App">   * means below: on any other route which is not movieId or / show the notFound component
+  <Router>
       <Header />
-      <Home />
-
-      Start here.
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/:movieId' element={<Movie />} />
+        <Route path='/*' element={<NotFound />} />
+      </Routes>
       <GlobalStyle />
-    </div>
-  );
-}
+  </Router>
+);
 
 export default App;
