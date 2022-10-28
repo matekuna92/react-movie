fdc146980bfbac498325b519ff0d16bb1da052a5
matekuna92 <kunamate66@gmail.com>
2022-10-28 20:42:54 +0200

    Home Page routes, movie thumbnails routes

--- src/components/Thumb/index.js
+++ src/components/Thumb/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 // styles
 import { Image } from './thumb.styles';
+import { Link } from 'react-router-dom';
 
-const Thumb = ({ image, movieId, cilckable }) => (
+const Thumb = ({ image, movieId, clickable }) => (
   <div>
-    <Image src={image} alt='Movie Thumb' />
+    {clickable ? (    // on Home page the thumbnails of movies should be clickable to go to the movieId url, but we also use this
+                      // Thumb component for the movie poster on each individual Movie page, where it should not be clickable
+      <Link to={`/${movieId}`}>
+        <Image src={image} alt='Movie Thumb' />
+      </Link>
+    ) : (
+      <Image src={image} alt='Movie Thumb' />
+    )}
   </div>
 )
 
